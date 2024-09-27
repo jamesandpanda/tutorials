8ccb1366e2e3474eccf6dfcddb54b5d7591c20ea
James Gunawan <z5421027@ad.unsw.edu.au>
2024-09-27 16:15:28 +1000

    complete wondrous example

--- COMP2511/24T3/tute03/src/wondrous/Wondrous.java
+++ COMP2511/24T3/tute03/src/wondrous/Wondrous.java
@@ -5,17 +5,26 @@ import java.util.List;
 
 public class Wondrous {
     public List<Integer> wondrous(int start) {
+        if (start < 1) {
+            throw new IllegalArgumentException("start has to be positive");
+        }
+
         int current = start;
         List<Integer> sequence = new ArrayList<Integer>();
 
+        if (start == 1) {
+            return sequence;
+        }
+
         while (current != 1) {
             sequence.add(current);
-            if (current % 2 == 0) {
+            if (current % 2 == 1) {
                 current = (current * 3) + 1;
             } else {
                 current = current / 2;
             }
         }
+        sequence.add(1);
 
         return sequence;
     }
