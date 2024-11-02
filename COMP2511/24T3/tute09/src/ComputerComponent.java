81360cd04084a78e6a03aa6d8606d257e006e609
James Gunawan <z5421027@ad.unsw.edu.au>
2024-11-02 23:46:45 +1100

    add tute09 files

---
+++ COMP2511/24T3/tute09/src/ComputerComponent.java
@@ -0,0 +1,2 @@
+public interface ComputerComponent {
+}
---
+++ COMP2511/24T3/tute09/src/Mouse.java
@@ -0,0 +1,12 @@
+public class Mouse implements ComputerComponent {
+    private String name;
+
+    public Mouse(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
