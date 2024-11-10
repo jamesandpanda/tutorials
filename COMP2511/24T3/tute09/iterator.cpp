d04d1231742c0bad42b7107046a379637f32da94
James Gunawan <z5421027@ad.unsw.edu.au>
2024-11-11 01:15:35 +1100

    edit annotations

--- COMP2511/24T3/tute09/iterator.cpp
+++ COMP2511/24T3/tute09/iterator.cpp
@@ -2,6 +2,7 @@
  * No prior knowledge of C++ should be necessary to understand the point of this example.
  * 
  * A C++ program to demonstrate the iterator pattern 'in the wild'.
+ * Java uses essentially the same idea, but hidden from the user.
  * In order to de-couple algorithms from the actual type of container (to keep data encapsulated and
  * prevent having to create separate implementations for different data structures), algorithms in
  * modern languages are operated on the iterators (i.e. abstract pointers) and how they traverse through
@@ -23,9 +24,10 @@ int main(void) {
     auto v = std::vector<int>{-45, 23, 4, 2, 6}; // List<Integer> v = new ArrayList<Integer>(-45, 23, 4, 2, 6);
 
     // To get an iterator representing the start of a container in C++, .begin() is used.
-    // To get an iterator representing the end of a container in C++, .end() is used.
-    // EXTRA NOTE: .end() does NOT point to the last element, but rather something representing
-    // one PAST the last element, like NULL in a C linked list.
+    // To get an iterator representing the start of a container in Java, .iterator() is used.
+
+    // The end of iteration in C++ is represented as .end(), which is itself an iterator.
+    // The end of iteration in Java is represented as iterator.hasNext() returning false.
 
     // These sorting algorithms are not implemented for arrays and vectors separately...
     // They operate on the actual iterators!
@@ -34,6 +36,7 @@ int main(void) {
 
     std::sort(v.begin(), v.end()); // Collections.sort(v);
 
+    // This for-of loop itself is using the iterator to traverse through the collection! Same as in Java.
     for (int num : a) {
         std::cout << num << " "; // System.out.print(num + " ");
     }
