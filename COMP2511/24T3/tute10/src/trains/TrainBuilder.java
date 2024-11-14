dcb9e631e5e7d5f159e06b076bd74fb463db067f
James Gunawan <z5421027@ad.unsw.edu.au>
2024-11-15 05:28:32 +1100

    add tute10 files

---
+++ COMP2511/24T3/tute10/src/trains/TrainBuilder.java
@@ -0,0 +1,29 @@
+package trains;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainBuilder {
+    private List<Wagon> cars = new ArrayList<>();
+
+    public void addPassengerWagon() {
+        // TODO
+    }
+
+    public void addCargoWagon() {
+        // TODO
+    }
+
+    public void addHighPoweredEngine() {
+        // TODO
+    }
+
+    public void addEngine() {
+        // TODO
+    }
+
+    public Train build() {
+        return null;
+    }
+}
+
