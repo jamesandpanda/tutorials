1ec6677fc966ea76dd1648e27dd3f2ce2a241978
James Gunawan <z5421027@ad.unsw.edu.au>
2025-03-03 19:49:55 +1100

    week 3: init files

---
+++ COMP2511/25T1/T09A/tute03/src/wondrous/Wondrous.java
@@ -0,0 +1,22 @@
+package wondrous;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Wondrous {
+    public List<Integer> wondrous(int start) {
+        int current = start;
+        List<Integer> sequence = new ArrayList<Integer>();
+
+        while (current != 1) {
+            sequence.add(current);
+            if (current % 2 == 0) {
+                current /= 2;
+            } else {
+                current = (current * 3) + 1;
+            }
+        }
+
+        return sequence;
+    }
+}
