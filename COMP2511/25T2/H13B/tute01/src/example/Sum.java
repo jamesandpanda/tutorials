6ebaffd7a7a488f5d1f54442c908f3fc39c19cea
James Gunawan <z5421027@ad.unsw.edu.au>
2025-06-05 15:19:51 +1000

    week 1: fix name of split output

--- COMP2511/25T2/H13B/tute01/src/example/Sum.java
+++ COMP2511/25T2/H13B/tute01/src/example/Sum.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        // 1 4 5 2
+        // We will receive an input of the form "a b c d e" from the user.
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
         sc.close();
 
         // line = "1 4 5 2"
         // .split(" ") = ["1", "4", "5", "2"]
-        String[] lines = line.split(" ");
+        String[] nums = line.split(" ");
         int sum = 0;
-        for (String num : lines) {
+        for (String num : nums) {
             sum += Integer.parseInt(num);
             // parseInt turns: "5" -> 5
         }
