bce06d3d5873e720ef51feb4f6a697d35edcc034
James Gunawan <z5421027@ad.unsw.edu.au>
2025-06-22 04:49:25 +1000

    week 4: init week 4 files

---
+++ COMP2511/starter/tute04/src/restaurant/strategies/ChargingStrategy.java
@@ -0,0 +1,17 @@
+package restaurant.strategies;
+
+import java.util.List;
+
+import restaurant.Meal;
+
+public interface ChargingStrategy {
+    /**
+     * The cost of a meal.
+     */
+    public double cost(List<Meal> order, boolean payeeIsMember);
+
+    /**
+     * Modifying factor of charges for customers.
+     */
+    public double getModifier(boolean payeeIsMember);
+}
