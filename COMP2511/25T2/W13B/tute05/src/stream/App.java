4cb26a19e9ab580a8d39025c1524fe2e8e965106
James Gunawan <z5421027@ad.unsw.edu.au>
2025-07-01 17:17:33 +1000

    week 5: init week 5 files

---
+++ COMP2511/25T2/W13B/tute05/src/stream/App.java
@@ -0,0 +1,20 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<String> strings = List.of("1", "2", "3", "4", "5");
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        List<String> strings2 = List.of("1", "2", "3", "4", "5");
+        List<Integer> ints = new ArrayList<Integer>();
+        for (String string : strings2) {
+            ints.add(Integer.parseInt(string));
+        }
+        System.out.println(ints);
+    }
+}
