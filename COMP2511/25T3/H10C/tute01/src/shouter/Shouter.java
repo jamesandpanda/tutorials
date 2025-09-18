2a42c99b3e0410d3e7cd2df6709d7055f0e8dc4d
James Gunawan <z5421027@ad.unsw.edu.au>
2025-09-18 14:54:51 +1000

    week 1: add annotations

--- COMP2511/25T3/H10C/tute01/src/shouter/Shouter.java
+++ COMP2511/25T3/H10C/tute01/src/shouter/Shouter.java
@@ -1,20 +1,27 @@
 package shouter;
 
 public class Shouter {
+    // Declaring an instance field (each shouter will store
+    // its own message)
     private String message;
 
+    // Constructor
     public Shouter(String message) {
         this.message = message;
     }
 
+    // Defining an instance method - we can invoke this method
+    // from objects of type Shouter.
     public void shout() {
         System.out.println(message.toUpperCase());
     }
 
+    // Getter
     public String getMessage() {
         return message;
     }
 
+    // Setter
     public void setMessage(String message) {
         this.message = message;
     }
--- COMP2511/25T3/H10C/tute01/src/sum/Sum.java
+++ COMP2511/25T3/H10C/tute01/src/sum/Sum.java
@@ -8,18 +8,23 @@ public class Sum {
         String s = sc.nextLine();
         sc.close();
 
-        // 1 2 3 4 5
-        // [1, 2, 3, 4, 5]
+        // "1 2 3 4 5"
         // .split(" ")
+        // -> ["1", "2", "3", "4", "5"]
         String[] nums = s.split(" ");
 
         int sum = 0;
 
         // C-style for loop
-        for (int i = 0; i < nums.length; i++) {
-            sum += Integer.parseInt(nums[i]);
-        }
+        // You may need to use this if you need to use array indexes
+        // outside of just accessing specific elements in the array.
+        // for (int i = 0; i < nums.length; i++) {
+        //     sum += Integer.parseInt(nums[i]);
+        // }
 
+        // "Enhanced for loop"/"for-each" loop
+        // Preferred when the C-style for loop does the exact same thing!
+        // This just loops over all elements in the array.
         for (String num : nums) {
             sum += Integer.parseInt(num);
         }
