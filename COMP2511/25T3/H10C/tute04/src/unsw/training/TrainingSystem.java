6a6b28d0e7e21175b0f4bb918d1d8663d73126b5
James Gunawan <z5421027@ad.unsw.edu.au>
2025-10-09 11:10:10 +1100

    week 4: add tute 4 code

--- COMP2511/25T3/H10C/tute04/src/unsw/training/TrainingSystem.java
+++ COMP2511/25T3/H10C/tute04/src/unsw/training/TrainingSystem.java
@@ -6,18 +6,14 @@ import java.util.List;
 public class TrainingSystem {
     private List<Trainer> trainers;
 
-    public LocalDate bookTraining(String employee, List<LocalDate> availability) {
+    public LocalDate bookTraining(String employee, List<LocalDate> availabilities) {
         for (Trainer trainer : trainers) {
-            for (Seminar seminar : trainer.getSeminars()) {
-                for (LocalDate available : availability) {
-                    if (seminar.getStart().equals(available) &&
-                            seminar.getAttendees().size() < 10) {
-                        seminar.getAttendees().add(employee);
-                        return available;
-                    }
-                }
+            LocalDate result = trainer.bookTraining(employee, availabilities);
+            if (result != null) {
+                return result;
             }
         }
+
         return null;
     }
 }
