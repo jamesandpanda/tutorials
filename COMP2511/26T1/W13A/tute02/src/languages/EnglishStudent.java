77dcac0f3a0e9e628ea2f87c12ad2c5c67c38d64
James Gunawan <z5421027@ad.unsw.edu.au>
2026-02-25 16:04:25 +1100

    add polymorphism example

--- COMP2511/26T1/W13A/tute02/src/languages/EnglishStudent.java
+++ COMP2511/26T1/W13A/tute02/src/languages/EnglishStudent.java
@@ -4,4 +4,9 @@ public class EnglishStudent extends Student {
     public EnglishStudent(String name) {
         super(name);
     }
+
+    @Override
+    public void sayHi() {
+        System.out.println(getName() + " says: Hello!");
+    }
 }
--- COMP2511/26T1/W13A/tute02/src/languages/Student.java
+++ COMP2511/26T1/W13A/tute02/src/languages/Student.java
@@ -2,7 +2,7 @@ package languages;
 
 import java.util.List;
 
-public class Student {
+public abstract class Student {
     private String name;
 
     public Student(String name) {
@@ -13,7 +13,10 @@ public class Student {
         return name;
     }
 
-    // TODO: Improve this code!
+    // we define an abstract method that each of the concrete students will
+    // implement on their own, according to what language they are learning
+    public abstract void sayHi();
+
     public static void main(String[] args) {
         Student a = new ChineseStudent("Alice");
         Student b = new EnglishStudent("Bob");
@@ -22,17 +25,23 @@ public class Student {
 
         List<Student> students = List.of(a, b, c, d);
 
+        // old ugly code!! :nauseated_face:
+        // for (Student s : students) {
+        //     if (s instanceof ChineseStudent) {
+        //         System.out.println(s.getName() + " says: 你好!");
+        //     } else if (s instanceof EnglishStudent) {
+        //         System.out.println(s.getName() + " says: Hello!");
+        //     } else if (s instanceof FrenchStudent) {
+        //         System.out.println(s.getName() + " says: Bonjour!");
+        //     } else if (s instanceof SpanishStudent) {
+        //         System.out.println(s.getName() + " says: ¡Hola!");
+        //     }
+        //     // and so on and so forth...
+        // }
+
+        // new clean code!! :heart_eyes:
         for (Student s : students) {
-            if (s instanceof ChineseStudent) {
-                System.out.println(s.getName() + " says: 你好!");
-            } else if (s instanceof EnglishStudent) {
-                System.out.println(s.getName() + " says: Hello!");
-            } else if (s instanceof FrenchStudent) {
-                System.out.println(s.getName() + " says: Bonjour!");
-            } else if (s instanceof SpanishStudent) {
-                System.out.println(s.getName() + " says: ¡Hola!");
-            }
-            // and so on and so forth...
+            s.sayHi();
         }
     }
 }
