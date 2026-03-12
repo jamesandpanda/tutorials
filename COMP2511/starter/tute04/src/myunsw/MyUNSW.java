6cdb1f2bd88dce87c87af3e4c123b70b3c7b32ad
James Gunawan <z5421027@ad.unsw.edu.au>
2026-03-12 14:49:18 +1100

    update myunsw example

--- COMP2511/starter/tute04/src/myunsw/MyUNSW.java
+++ COMP2511/starter/tute04/src/myunsw/MyUNSW.java
@@ -1,18 +1,21 @@
 package myunsw;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
 public class MyUNSW {
-    private static int nextStudentId = 5000000;
-
-    private static final Map<String, Integer> programs = Map.of(
+    public static final Map<String, Integer> PROGRAMS = Map.of(
         "computer science", 3778,
         "engineering", 3707,
         "commerce", 3502,
         "medicine", 3805
     );
+
+    private static List<Student> students = new ArrayList<>();
+    private static int nextStudentId = 5000000;
 
     public static void main(String[] args) {
         System.out.println("Choose a program to enrol a new student into.");
@@ -22,15 +25,15 @@ public class MyUNSW {
         while (sc.hasNextLine()) {
             String program = sc.nextLine().toLowerCase();
 
-            if (!programs.containsKey(program)) {
+            if (!PROGRAMS.containsKey(program)) {
                 System.out.println("Invalid program given.");
             } else {
                 int id = nextStudentId;
                 ++nextStudentId;
-                int programCode = programs.get(program);
+                int programCode = PROGRAMS.get(program);
                 int luck = new Random().nextInt(1, 11);
 
-                Student s = new Student(id, programCode, luck);
+                students.add(new Student(id, programCode, luck));
                 System.out.println("Enrolled new student with id " + id + " in program " + programCode + " with " + luck + " luck.");
             }
         }
