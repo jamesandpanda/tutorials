aeb1cd9493d2b54385ff90fbcafb027354eee17b
James Gunawan <z5421027@ad.unsw.edu.au>
2026-09-24 16:33:35 +1000

    add annotations

--- COMP2511/26T3/tute02/README.md
+++ COMP2511/26T3/tute02/README.md
@@ -1,3 +1,3 @@
 # Tutorial 02
 
-See https://cgi.cse.unsw.edu.au/~cs2511/25T2/tutorials/tute02
+See https://cgi.cse.unsw.edu.au/~cs2511/26T3/tutorials/tute02
--- COMP2511/26T3/tute02/src/pets/Pet.java
+++ COMP2511/26T3/tute02/src/pets/Pet.java
@@ -3,7 +3,6 @@ package pets;
 import java.util.List;
 
 public abstract class Pet {
-    // TODO: Improve the quality of the code
     private String name;
 
     public Pet(String name) {
@@ -14,11 +13,16 @@ public abstract class Pet {
         return name;
     }
 
+    // abstract method - don't have to provide an implementation in this class
+    // the concrete subclasses will implement this
     public abstract void makeNoise();
 
     public static void main(String[] args) {
         List<Pet> pets = List.of(new Bird("Blue"), new Cat("Cookie"), new Dog("Duke"));
 
+        // way better than the original code which used instanceof to type-check
+        // and then print the appropriate string depending on it!
+        // this is polymorphism at play :)
         for (Pet p : pets) {
             p.makeNoise();
         }
